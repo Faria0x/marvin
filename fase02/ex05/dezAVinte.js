7943f0b30e66e3d2d5e287f255654d88d7f21887
lucasfaria10 <59841015+lucasfaria10@users.noreply.github.com>
2020-01-16 16:22:12 -0300

    Numero aleatorio entre 10e 20

--- fase02/ex05/dezAVinte.js
+++ fase02/ex05/dezAVinte.js
@@ -1,5 +1,9 @@
-function dezAVinte() {
-    min= Math.ceil(20)
-    max= Math.floor(10)
-    return Math.floor(Math.random() * (20 - 10 + 1) +10)
+function dezAVinte(){
+
+min= Math.ceil(20)
+
+max= Math.floor(10)
+
+return Math.floor(Math.random() * (20 - 10 + 1) +10)
+
 }
