c244f24f2f38318b45399c2e89ef4d06255ad58e
lucasfaria10 <59841015+lucasfaria10@users.noreply.github.com>
2020-01-20 14:42:00 -0300

    Achar número e frases para cada intervalo

---
+++ fase03/ex05/acharNumero.js
@@ -0,0 +1,10 @@
+function acharNumero (numero){
+if (numero < 5) 
+return numero = "mercurio"
+if (numero >= 5 && numero <=10)
+return numero = "venus"
+if (numero >10 && numero <=100)
+return numero = "terra"
+if (numero >100)
+return numero = "marte"
+}
