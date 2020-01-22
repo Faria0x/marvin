0536b64743d7d5e60991a5267e5cfb8e131e2b12
lucasfaria10 <59841015+lucasfaria10@users.noreply.github.com>
2020-01-22 17:04:38 -0300

    Fizzbuzz multiplos de 3 e 5

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,13 +1,25 @@
 function fizzbuzz(n1, n2){
+
     while (n1<=n2){
-        var str = " ";
+
+        var str =" ";
+
         if (n1 % 3 === 0) {
+
             str += "Fizz";
+
         }
+
         if (n1 % 5 === 0) {
+
             str += "Buzz";
+
         }
+
             console.log(n1+ str)
+
         n1++
+
     }
+
 }
