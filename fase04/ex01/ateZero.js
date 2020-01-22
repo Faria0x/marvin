032ada7ba7c9dadddd0520bdacf197c242c99bab
lucasfaria10 <59841015+lucasfaria10@users.noreply.github.com>
2020-01-22 17:15:59 -0300

    Contagem até o 0

--- fase04/ex01/ateZero.js
+++ fase04/ex01/ateZero.js
@@ -1,13 +1,12 @@
+
+
 function ateZero(num){
 var a = num;
 var b = 0;
 var c = [];
     while (b<a){
           c.push(b++)}
-        
     while (b>=a){    
-            c.push(b--)}
-
+            c.push(a++)}
     return c
-    
 }
